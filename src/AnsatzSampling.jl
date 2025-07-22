2c306909fb75f51889ca1f0b2f4da06681584e25
mtsch <matijacufar@gmail.com>
2025-07-22 13:30:38 +1200

    Implement adjoints

--- src/AnsatzSampling.jl
+++ src/AnsatzSampling.jl
@@ -1,12 +1,21 @@
-struct AnsatzSampling{T,N,A<:AbstractAnsatz{<:Any,T,N},H} <: ModifiedHamiltonian{T}
+using Rimu.Hamiltonians: ModifiedHamiltonian, TransformUndoer, parent_operator
+
+struct AnsatzSampling{Adj,T,N,A<:AbstractAnsatz{<:Any,T,N},H} <: ModifiedHamiltonian{T}
     hamiltonian::H
     ansatz::A
     params::SVector{N,T}
+
+    function AnsatzSampling{Adj}(
+        hamiltonian::H, ansatz::A, params::SVector{N,T}
+    ) where {Adj,T,N,A<:AbstractAnsatz{<:Any,T,N},H}
+        return new{Adj,T,N,A,H}(hamiltonian, ansatz, params)
+    end
 end
 
 function AnsatzSampling(
     h, ansatz::AbstractAnsatz{<:Any,<:Any,N}, params::Vararg{Number,N}
 ) where {N}
+
     return AnsatzSampling(h, ansatz, params)
 end
 
@@ -21,24 +30,33 @@ function AnsatzSampling(h, ansatz::AbstractAnsatz{K,T,N}, params) where {K,T,N}
 
     params = SVector{N,T}(params)
 
-    return AnsatzSampling(h, ansatz, params)
+    return AnsatzSampling{false}(h, ansatz, params)
 end
 
 Rimu.Hamiltonians.parent_operator(h::AnsatzSampling) = h.hamiltonian
 Rimu.Hamiltonians.modify_diagonal(h::AnsatzSampling, _, value) = value
 
-function Rimu.Hamiltonians.modify_offdiagonal(h::AnsatzSampling, src, dst, value)
+Rimu.LOStructure(h::AnsatzSampling) = LOStructure(parent_operator(h))
+function Rimu.adjoint(h::AnsatzSampling{A}) where {A}
+    return AnsatzSampling{!A}(h.hamiltonian', h.ansatz, h.params)
+end
+
+function Rimu.Hamiltonians.modify_offdiagonal(h::AnsatzSampling{A}, src, dst, value) where {A}
     src_ansatz = (h.ansatz)(src, h.params)
     dst_ansatz = (h.ansatz)(dst, h.params)
-    return dst => ansatz_modify(matrix_element, src_ansatz, dst_ansatz)
+    if !A
+        return dst => value * (dst_ansatz / src_ansatz)
+    else
+        return dst => value * (src_ansatz / dst_ansatz)
+    end
 end
 
 ###
 ### TransformUndoer
 ###
-const AnsatzTransformUndoer{O} = Rimu.Hamiltonians.TransformUndoer{<:Any,<:AnsatzSampling,O}
+const AnsatzTransformUndoer{O} = TransformUndoer{<:Any,<:AnsatzSampling,O}
 
-function Rimu.Hamiltonians.TransformUndoer(k::AnsatzSampling, op::AbstractOperator)
+function TransformUndoer(k::AnsatzSampling, op::AbstractOperator)
     T = promote_type(eltype(k) * eltype(op))
     return Rimu.Hamiltonians.TransformUndoer{T,typeof(k),typeof(op)}(k, op)
 end
